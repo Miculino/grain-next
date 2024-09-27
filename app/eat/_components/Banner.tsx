2da1e30420b476702616c524f8617293a448b35f
RemusDB <remusd.buhaianu@gmail.com>
2024-09-27 09:21:14 +0300

    added banner content

--- app/eat/_components/Banner.tsx
+++ app/eat/_components/Banner.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
 import React from "react";
 
 export default function Banner() {
   return (
-    <div>
-      <Image
-        src={
-          "https://storage.googleapis.com/spineproduction/uploads/banner/picture/134/picture.jpeg"
-        }
-        role="presentation"
-        alt=""
-        width={500}
-        height={500}
-      />
+    <div
+      className={`bg-[url("https://storage.googleapis.com/spineproduction/uploads/banner/picture/134/picture.jpeg")] w-full bg-cover`}
+    >
+      <div>
+        <p className="text-4xl">Can you keep a secret?</p>
+        <p className="text-lg">
+          Word around the kitchen is a Michelin-starred pastry chef made this
+          just for us. Can’t reveal her name, but we can tell you that our
+          newest dessert is almost too good to share. Almost.
+        </p>
+      </div>
     </div>
   );
 }
