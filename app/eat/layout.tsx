83cdc07e5ed9a47a196a8cea50df11487cd3a88a
RemusDB <remusd.buhaianu@gmail.com>
2024-09-28 19:21:02 +0300

    add: close modal on escape

--- app/eat/layout.tsx
+++ app/eat/layout.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+import useModalStore from "../store/useModalStore";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { closeModal } = useModalStore();
+
+  useEffect(() => {
+    const handleCloseModalOnEscape = () => {
+      closeModal();
+    };
+
+    window.addEventListener("keydown", handleCloseModalOnEscape);
+
+    return () => {
+      window.removeEventListener("keydown", handleCloseModalOnEscape);
+    };
+  }, []);
+
   return <div>{children}</div>;
 }
