e8f873f53bef9ff3cc1c02c4f88697df0ae845ad
RemusDB <remusd.buhaianu@gmail.com>
2024-10-01 13:12:55 +0300

    toggle between pick up and delivery

--- app/eat/_components/OrderFulfillmentOptions.tsx
+++ app/eat/_components/OrderFulfillmentOptions.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import OrderNavigation from "./OrderNavigation";
 import useAddressStore from "@/app/store/useAddressStore";
-import useModalStore from "@/app/store/useModalStore";
 import Autocomplete from "react-google-autocomplete";
 import Button from "./Button";
 
@@ -9,16 +8,11 @@ export default function OrderFulfillmentOptions() {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
   const [isSearchEnabled, setIsSearchEnabled] = useState<boolean>(false);
   const [selectedPlace, setSelectedPlace] = useState<string>("");
+  const [selectedAddressType, setSelectedAddressType] = useState<string>("");
 
-  const {
-    deliveryAddress,
-    selectedAddressType,
-    setSelectedAddressType,
-    setDeliveryAddress,
-  } = useAddressStore();
+  const { deliveryAddress, setDeliveryAddress } = useAddressStore();
 
   const handleDropdownToggle = () => {
-    setSelectedAddressType("delivery");
     setIsDropdownOpen((prev) => !prev);
     setIsSearchEnabled(false); // Reset search state when toggling the dropdown
   };
@@ -37,9 +31,14 @@ export default function OrderFulfillmentOptions() {
   const handleAddDeliveryAddress = (event: React.MouseEvent) => {
     event.stopPropagation();
     if (selectedPlace) {
+      // Set the selected address type based on the dropdown state
       setDeliveryAddress(selectedPlace);
       setIsSearchEnabled(false);
       setIsDropdownOpen(false);
+      // Toggle the address type based on previous selection
+      setSelectedAddressType((prev) =>
+        prev === "pick_up" ? "delivery" : "pick_up"
+      );
     }
   };
 
@@ -49,61 +48,76 @@ export default function OrderFulfillmentOptions() {
         <p className="mb-1 text-sm">Ready to rock and roll!</p>
         <div className="w-full h-1 rounded-md bg-green-600"></div>
         <div className="flex items-center justify-between mt-2">
-          <p className="text-sm">Pick up from Food Point</p>
+          <p className="text-sm">
+            {selectedAddressType === "delivery"
+              ? "Delivery fee"
+              : "Pick up from Food Point"}
+          </p>
           <span className="text-sm">FREE</span>
         </div>
         <OrderNavigation
           onClick={handleDropdownToggle}
-          text="Or get door-to-door delivery"
+          text={
+            selectedAddressType === "pick_up"
+              ? "Or get door-to-door delivery"
+              : "Pick up for free at the nearest Food Point"
+          }
           small
           isDropdownOpen={isDropdownOpen}
         />
-        {isDropdownOpen &&
-          (selectedAddressType === "delivery" ? (
-            <div className="shadow-sm">
+        {isDropdownOpen && (
+          <div className="shadow-sm">
+            {/* Show Delivery Address when Pickup is selected */}
+            {selectedAddressType === "pick_up" && deliveryAddress && (
               <div className="bg-white border-b border-b-gray p-2 cursor-pointer hover:font-bold hover:bg-[#f7f7f7]">
                 <p className="text-xs">{deliveryAddress}</p>
               </div>
-              <div
-                onClick={handleToggleSearch}
-                className="bg-white p-2 cursor-pointer"
-              >
-                {!isSearchEnabled ? (
-                  <p className="text-xs underline">Add new address</p>
-                ) : (
-                  <div onClick={(e) => e.stopPropagation()}>
-                    <Autocomplete
-                      apiKey="AIzaSyDz9wEqFecagZ1tGabdzQzLUEE7JHdbMSs"
-                      options={{
-                        componentRestrictions: { country: "SG" }, // Restrict to Singapore
-                        types: ["address"], // Specify types to get address locations
-                      }}
-                      placeholder="Please enter three or more characters"
-                      onPlaceSelected={handlePlaceSelected}
-                      style={{
-                        width: "100%",
-                        padding: "8px 8px",
-                        border: "1px solid #bfbfbf",
-                      }} // Optional: Style for better UX
-                    />
-                    <Button
-                      onClick={handleAddDeliveryAddress}
-                      disabled={!selectedPlace}
-                      intent={selectedPlace ? "primary" : "disabled"}
-                      className="py-2"
-                      size={"full"}
-                    >
-                      Add address
-                    </Button>
-                  </div>
-                )}
+            )}
+
+            {/* Search for new address */}
+            <div
+              onClick={handleToggleSearch}
+              className="bg-white p-2 cursor-pointer"
+            >
+              {!isSearchEnabled ? (
+                <p className="text-xs underline">Add new address</p>
+              ) : (
+                <div onClick={(e) => e.stopPropagation()}>
+                  <Autocomplete
+                    apiKey="AIzaSyDz9wEqFecagZ1tGabdzQzLUEE7JHdbMSs"
+                    options={{
+                      componentRestrictions: { country: "SG" }, // Restrict to Singapore
+                      types: ["address"], // Specify types to get address locations
+                    }}
+                    placeholder="Please enter three or more characters"
+                    onPlaceSelected={handlePlaceSelected}
+                    style={{
+                      width: "100%",
+                      padding: "8px 8px",
+                      border: "1px solid #bfbfbf",
+                    }} // Optional: Style for better UX
+                  />
+                  <Button
+                    onClick={handleAddDeliveryAddress}
+                    disabled={!selectedPlace}
+                    intent={selectedPlace ? "primary" : "disabled"}
+                    className="py-2"
+                    size={"full"}
+                  >
+                    Add address
+                  </Button>
+                </div>
+              )}
+            </div>
+
+            {/* Show Pick Up Point when Delivery is selected */}
+            {selectedAddressType === "delivery" && (
+              <div className="bg-white p-2">
+                <p className="text-xs">Pick up point</p>
               </div>
-            </div>
-          ) : (
-            <div>
-              <p>pick up point</p>
-            </div>
-          ))}
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
